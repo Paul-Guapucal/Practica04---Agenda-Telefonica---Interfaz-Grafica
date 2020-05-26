5648a8d06b73dc7c9e67485ede46d13a5aeeec2a
Paul-Guapucal <64905748+Paul-Guapucal@users.noreply.github.com>
2020-05-26 18:29:19 -0500

    Proceso de la Interfaz

--- src/ec/edu/ups/vista/VentanaAgenda.java
+++ src/ec/edu/ups/vista/VentanaAgenda.java
@@ -1,11 +1,9 @@
-package ec.edu.ups.vista;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package ec.edu.ups.vista;
 
 import java.awt.*;
 
